43f7c45897c33f2709e20e9f2a34e18810f2619a
Fernando Almeida <fernando@fernandoalmeida.net>
2011-08-02 02:44:40 -0300

    Changed Prawn to PDFKit

--- lib/pdf_renderer.rb
+++ lib/pdf_renderer.rb
@@ -1,12 +1,24 @@
 module PdfRenderer
   require 'action_controller'
-  require 'prawn'
+  require 'pdfkit'
   
   Mime::Type.register 'application/pdf', :pdf
   
   ActionController::Renderers.add :pdf do |filename, options|
-    pdf = Prawn::Document.new
-    pdf.text render_to_string(options)
-    send_data(pdf.render, :filename => "#{filename}.pdf", :type => "application/pdf", :disposition => "attachment")
+    layout   = options[:layout] || "application.html.erb"
+    template = "/#{controller_name}/#{action_name}.html.erb"
+    html     = render_to_string(:layout => layout, :template => template)
+    kit      = PDFKit.new(html, DEFAULT_OPTIONS)
+    send_data(kit.to_pdf, :filename => "#{filename || request.path.gsub('/','_')}.pdf", :type => 'application/pdf', :disposition => 'attachment')
   end
+
+  DEFAULT_OPTIONS = {
+    :page_size          => 'A4',
+    :print_media_type   => true,
+    :margin_left        => 4,
+    :margin_right       => 2,
+    :margin_top         => 2,
+    :margin_bottom      => 2
+  }
+
 end
--- test/integration/navigation_test.rb
+++ test/integration/navigation_test.rb
@@ -12,7 +12,9 @@ class NavigationTest < ActiveSupport::IntegrationCase
   end
 
   test 'pdf renderer uses a specific template' do
-    visit "/another.pdf"
+    visit home_path
+    click_link 'PDF'
+
     assert_equal 'binary', headers['Content-Transfer-Encoding']
     assert_equal 'attachment; filename="contents.pdf"', headers['Content-Disposition']
     assert_equal 'application/pdf', headers['Content-Type']
